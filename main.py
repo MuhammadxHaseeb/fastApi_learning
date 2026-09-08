bbe3dd1ed842d90b958b85944cd13720f7c6869e
Muhammad Haseeb <haseebmuhammad578@gmail.com>
2026-09-08 15:03:43 +0500

    4. Path Parameters and Query Parameters

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request 
+from mockData import products
 
 app = FastAPI()
 
@@ -9,6 +10,36 @@ app = FastAPI()
 def home():
     return "Welcome to Fastapi Series !"
 
-@app.get("/connect")
-def contact():
-    return "Connect any time"
+
+@app.get("/products")
+def get_products():
+    return products
+
+# path Parameters
+@app.get("/product/{product_id}")
+def get_products(product_id:int):
+    for i in products:
+        if product_id == i.get("id"):
+            return i
+    
+    return {
+        "error":"Product not found"
+    }
+
+# query parameter
+@app.get("/greet")
+def greet_user(name:str, age:int):
+    return {
+        "greet": f"Hello {name}, Your age is {age}"
+    }
+
+
+# query parameter via request
+@app.get("/greet2")
+def greet_user(request:Request):
+    
+    query_params= dict(request.query_params)
+
+    return {
+        "greet": f"Hello {query_params.get("name")}, Your age is {query_params.get("age")}"
+    }
---
+++ mockData.py
@@ -0,0 +1,15 @@
+products = [
+    {
+        "id":1,
+        "title":"Android Phone",
+        "price":1000,
+        "count":10
+    },
+
+    {
+        "id":2,
+        "title":"ios Phone",
+        "price":2000,
+        "count":15
+    }
+]
